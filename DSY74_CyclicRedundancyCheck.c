21ce7861b655da8aa8381b6ba47d9d318dc1b215
Pranav Sawant <pranavsawant2579@gmail.com>
2021-12-31 14:32:18 +0530

    Second Commit

--- DSY74_CyclicRedundancyCheck.c
+++ DSY74_CyclicRedundancyCheck.c
@@ -11,7 +11,7 @@ int main()
     system("cls");
 
     char data[6], div[4];
-    int cdata[20], cdiv[4], datacpy[20], i;
+    int cdata[20], cdiv[4], datacpy[20], i, k, temp0 = {0, 0, 0, 0, 0, 0, 0}, temp1 = {1, 1, 1, 1, 1, 1, 1}, x = 9;
 
     printf("\nEnter the data: ");
     scnaf("%s", &data);
@@ -22,12 +22,64 @@ int main()
         cdata[i] = data[i] - '0';
 
     for (i = 0; i < 6; i++)
+        cdiv[i] = div[i] - '0';
+
+    for (i = 0; i < 6; i++)
         datacpy[i] = cdata[i];
 
     for (i = 6; i < 9; i++)
         cdata[i] = 0;
 
     for (i = 0; i < 9; i++)
+    {
+        if (cdata[i] == cdiv[i])
+        {
+            k = i;
+            for (j = 0; j < 4; j++)
+            {
+                if (cdata[k] == cdiv[j])
+                {
+                    cdata[k] = 0;
+                    k++;
+                }
+            }
+        }
+        else if (cdata[i] == 1)
+        {
+            for (j = 0; j < 4; j++)
+            {
+                k = i;
+                if (cdata[k] == temp1[j])
+                {
+                    cdata[k] = 0;
+                    k++;
+                }
+                else
+                {
+                    cdata[k] = 1;
+                    k++;
+                }
+            }
+        }
+        else if (cdata[i] == 0)
+        {
+            for (j = 0; j < d; j++)
+            {
+                k = i;
+                if (cdata[k] == temp0[k])
+                {
+                    cdata[k] = 0;
+                    k++;
+                }
+                else
+                {
+                    cdata[k] = 1;
+                    k++;
+                }
+            }
+        }
+    }
+    for (i = 0; i < 3; i++)
     {
     }
 }
